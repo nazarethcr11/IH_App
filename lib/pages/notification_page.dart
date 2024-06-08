ec42756124b3e5eeac6ca2ba60706bb416f277b8
JoyceBautista <u201924504@upc.edu.pe>
2024-06-07 19:07:11 -0500

    notification view update

--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:inclusive_hue/components/my_notification.dart';
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({Key? key}) : super(key: key);
@@ -13,64 +14,41 @@ class NotificationPage extends StatelessWidget {
           style: TextStyle(
             fontSize: 20.0,
             fontWeight: FontWeight.bold,
-            color: Theme.of(context).colorScheme.primary,
+            color: Theme
+                .of(context)
+                .colorScheme
+                .primary,
           ),
         ),
         centerTitle: true,
       ),
-      backgroundColor: Theme.of(context).colorScheme.secondary,
-      body: Builder(
-        builder: (context) => ListView(
-          children: [
-            _buildNotificationContainer(
-              context,
-              'Usted realizó un cambio en el tipo de daltonismo: “Deuteranopia”',
-              'lib/images/logotype/logo.png', // Ruta de la imagen
-            ),
-            _buildNotificationContainer(
-              context,
-              'Usted lleva ejecutando la apliación por mas de 1 hora',
-              'lib/images/logotype/logo.png', // Ruta de la imagen
-            ),
-            _buildNotificationContainer(
-              context,
-              'Encendiste la aplcación. Esta se ejecutará en segundo plano ',
-              'lib/images/logotype/logo.png', // Ruta de la imagen
-            ),
-            // Agrega más mensajes según sea necesario
-          ],
-        ),
-      ),
-    );
-  }
 
-  Widget _buildNotificationContainer(BuildContext context, String message, String imagePath) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-      padding: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: Theme.of(context).colorScheme.surface,
-      ),
-      child: Row(
-        children: [
-          Image.asset(
-            imagePath,
-            width: 40.0, // Ancho de la imagen
-            height: 40.0, // Alto de la imagen
-          ),
-          SizedBox(width: 10.0),
-          Expanded( // Envolver el Row dentro de un Expanded
-            child: Text(
-              message,
-              style: TextStyle(
-                fontSize: 15,
-                color: Theme.of(context).colorScheme.tertiary,
-              ),
+      backgroundColor: Theme
+          .of(context)
+          .colorScheme
+          .secondary,
+      body: Builder(
+        builder: (context) =>
+            ListView(
+              children: [
+                MyNotification(
+                  message: 'Usted realizó un cambio en el tipo de daltonismo: “Deuteranopia”',
+                  imagePath: 'lib/images/logotype/logo.png',
+                ),
+                MyNotification(
+                  message: 'Usted realizó un cambio en el tipo de daltonismo: “Deuteranopia”',
+                  imagePath: 'lib/images/logotype/logo.png', // Ruta de la imagen
+                ),
+                MyNotification(
+                  message: 'Usted realizó un cambio en el tipo de daltonismo: “Deuteranopia”',
+                  imagePath: 'lib/images/logotype/logo.png', // Ruta de la imagen
+                ),
+                // Agrega más mensajes según sea necesario
+              ],
             ),
-          ),
-        ],
       ),
     );
   }
 }
+
+
---
+++ lib/pages/plan_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PlanPage extends StatelessWidget {
+  const PlanPage({Key? key}) : super(key: key);
+
+@override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
